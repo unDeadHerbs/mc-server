3fec169b120b6d7bd6b36508783f637018ba0cff
unDeadHerbs <undeadherbs@gmail.com>
2017-06-21 23:05:34 -0500

    drafted update worlds script

---
+++ updateWorlds.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# use
+#find -maxdepth 1 -type l|cut -d / -f 2|xargs 
+# to find a list of the world files
+# TODO: have that check that they link to /worlds
+
+# remove from /worlds any that are not linked to (removed by MultiVerse)
+
+# move into the /worlds folder any worlds found outside of it
+# (not sure how to do that, prehaps check untracked folders?)
+
+# create a simlink to every folder in /worlds
+# (don't re-make ones that already exist)
