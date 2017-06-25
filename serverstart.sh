42e9c59bbb024ad944a96fdc351c8fc9c9617645
unDeadHerbs <undeadherbs@gmail.com>
2017-06-25 11:30:54 -0500

    that seems like a more controllable method

--- serverstart.sh
+++ serverstart.sh
@@ -1,33 +1,41 @@
 #!/bin/sh
 
-git s
 git fetch
+git b -d running
+git b running
+git checkout running
 git merge --no-ff origin/next --no-edit
 git submodule sync
-git submodule --init --recursive
+git submodule init --recursive
 # run update worlds script
 cd worlds
 git s
-git checkout worlds
-git pull
-git pop
-git add --all
-git commit -m "auto commit on `date \"+%Y-%m-%d\"`"
-git push
 cd ..
 cd plugins
 git s
-git checkout plugins
-git pull
-git pop
-git add --all
-git commit -m "auto commit on `date \"+%Y-%m-%d\"`"
-git push
 cd ..
+git submodule update --recursive
+cd worlds
+git b -d worlds
+git b worlds
+git checkout worlds
 git pop
 git add --all
 git commit -m "auto commit on `date \"+%Y-%m-%d\"`"
-git push
+git push -f
+cd ..
+cd plugins
+git b -d plugins
+git b plugins
+git checkout plugins
+git pop
+git add --all
+git commit -m "auto commit on `date \"+%Y-%m-%d\"`"
+git push -f
+cd ..
+git add --all
+git commit -m "auto commit on `date \"+%Y-%m-%d\"`"
+git push -f
 git gc
 
 BINDIR=$(dirname "$(readlink -fn "$0")")
