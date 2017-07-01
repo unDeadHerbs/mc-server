7237be8e3708ff26d1b276c71146804ab5c7ca49
unDeadHerbs <undeadherbs@gmail.com>
2017-07-01 17:30:56 -0500

    added functions to run script

--- serverstart.sh
+++ serverstart.sh
@@ -1,51 +1,58 @@
 #!/bin/sh
 
-git fetch
-git s
-git checkout `git rev-parse --short HEAD`
-git rh
-git b -d running
-git b running
-git checkout running
-git pop
-git commit -m "auto commit on `date \"+%Y-%m-%d\"`"
-git merge --no-ff origin/next --no-edit
-git submodule sync
-git submodule init --recursive
-# run update worlds script
-cd worlds
-git s
-cd ..
-cd plugins
-git s
-cd ..
-git submodule update --recursive
-cd worlds
-git checkout `git rev-parse --short HEAD`
-git b -d worlds
-git b worlds
-git checkout worlds
-git pop
-git add --all
-git commit -m "auto commit on `date \"+%Y-%m-%d\"`"
-git push -f
-cd ..
-cd plugins
-git checkout `git rev-parse --short HEAD`
-git b -d plugins
-git b plugins
-git checkout plugins
-git pop
-git add --all
-git commit -m "auto commit on `date \"+%Y-%m-%d\"`"
-git push -f
-cd ..
-git pop
-git add --all
-git commit -m "auto commit on `date \"+%Y-%m-%d\"`"
-git push -f
-git gc
+update_server(){
+    git fetch
+    git s
+    git checkout `git rev-parse --short HEAD`
+    git rh
+    git b -d running
+    git b running
+    git checkout running
+    git pop
+    git commit -m "auto commit on `date \"+%Y-%m-%d\"`"
+    git merge --no-ff origin/next --no-edit
+    git submodule sync
+    git submodule init --recursive
+    # run update worlds script
+    cd worlds
+    git s
+    cd ..
+    cd plugins
+    git s
+    cd ..
+    git submodule update --recursive
+    cd worlds
+    git checkout `git rev-parse --short HEAD`
+    git b -d worlds
+    git b worlds
+    git checkout worlds
+    git pop
+    git add --all
+    git commit -m "auto commit on `date \"+%Y-%m-%d\"`"
+    git push -f
+    cd ..
+    cd plugins
+    git checkout `git rev-parse --short HEAD`
+    git b -d plugins
+    git b plugins
+    git checkout plugins
+    git pop
+    git add --all
+    git commit -m "auto commit on `date \"+%Y-%m-%d\"`"
+    git push -f
+    cd ..
+    git pop
+    git add --all
+    git commit -m "auto commit on `date \"+%Y-%m-%d\"`"
+    git push -f
+    git gc
+}
 
-BINDIR=$(dirname "$(readlink -fn "$0")")
-cd "$BINDIR"
-java -Xmx1024M -jar $(ls |grep spi|grep jar|sort -r|head -n 1) --log-append true
+start_server(){
+    BINDIR=$(dirname "$(readlink -fn "$0")")
+    cd "$BINDIR"
+    java -Xmx1024M -jar $(ls |grep spi|grep jar|sort -r|head -n 1) --log-append true
+}
+
+update_server
+start_server
